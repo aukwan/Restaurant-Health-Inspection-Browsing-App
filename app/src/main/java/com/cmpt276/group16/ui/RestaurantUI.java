da16103ce072bd5cf07d9c9acd92bf4625676ea6
skhetcho <khetcho@gmail.com>
2020-11-02 03:13:46 -0800

    Pulled branch 3-display-list-of-all-restaurants logic | Adapted current code with branch 3-display-list-of-all-restaurants | added icon for hazard level on the restaurant details | added elegant UI | removed top back

--- app/src/main/java/com/cmpt276/group16/ui/RestaurantUI.java
+++ app/src/main/java/com/cmpt276/group16/ui/RestaurantUI.java
@@ -4,11 +4,15 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.content.pm.ActivityInfo;
 import android.os.Bundle;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.TextView;
 
@@ -16,6 +20,7 @@ import com.cmpt276.group16.R;
 import com.cmpt276.group16.model.Inspection;
 import com.cmpt276.group16.model.RestaurantList;
 
+import java.lang.annotation.IncompleteAnnotationException;
 
 
 public class RestaurantUI extends AppCompatActivity {
@@ -24,6 +29,7 @@ public class RestaurantUI extends AppCompatActivity {
     private ArrayAdapter<Inspection> adapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_restaurant_u_i);
         extractDataFromSharedPref();
@@ -44,17 +50,22 @@ public class RestaurantUI extends AppCompatActivity {
 
     //setup the textview for UI
     private void setupTextViews(){
+        //get text values
         String restaurantName = restaurantManager.getRestArray().get(restaurantIndex).getName();
         String restaurantAddress = restaurantManager.getRestArray().get(restaurantIndex).getPhysicalCity() + ", " +
                 restaurantManager.getRestArray().get(restaurantIndex).getPhysicalAddress();
-        String restaurantGPS = restaurantManager.getRestArray().get(restaurantIndex).getLatitude() + "," +
+        String restaurantGPS = restaurantManager.getRestArray().get(restaurantIndex).getLatitude() + ", " +
                 restaurantManager.getRestArray().get(restaurantIndex).getLongitude();
-        TextView textView = (TextView) findViewById(R.id.restaurantName);
-        textView.setText(restaurantName);
-        textView = (TextView) findViewById(R.id.restaurantAddress);
-        textView.setText(restaurantAddress);
-        textView = (TextView) findViewById(R.id.restaurantGPS);
-        textView.setText(restaurantGPS);
+
+        //initialize TextViews
+        TextView restaurantNameTextView = (TextView) findViewById(R.id.restaurantName);
+        TextView restaurantAddressTextView = (TextView) findViewById(R.id.restaurantAddress);
+        TextView restaurantGPSTextView = (TextView) findViewById(R.id.restaurantGPS);
+
+        //set TextViews to the text value
+        restaurantNameTextView.setText(restaurantName);
+        restaurantAddressTextView.setText(restaurantAddress);
+        restaurantGPSTextView.setText(restaurantGPS);
     }
 
 
@@ -80,21 +91,37 @@ public class RestaurantUI extends AppCompatActivity {
         @Override
         public View getView(int position, View convertView, ViewGroup parent){
             View itemView = convertView;
+
             if (itemView == null){
                 itemView = getLayoutInflater().inflate(R.layout.issueslistview, parent, false);
             }
+
             Inspection currentInspection = restaurantManager.getRestArray().get(restaurantIndex).getInspectionList().get(position);
             //TODO: add icons for hazard level, and other stuff
 
-            String numCrit = "Critical issues#: " + Integer.toString(currentInspection.getNumCritical());
-            String numNonCrit = "Non - Critical issues#: " + Integer.toString(currentInspection.getNumNonCritical());
+            String numCrit = "Critical issues #: " + Integer.toString(currentInspection.getNumCritical());
+            String numNonCrit = "Non - Critical issues #: " + Integer.toString(currentInspection.getNumNonCritical());
             String inspectDate = "Inspection Date: " + Integer.toString(currentInspection.getInspectionDate());
-            TextView textView  = (TextView) itemView.findViewById(R.id.criticalIssuesListView);
-            textView.setText(numCrit);
-            textView  = (TextView) itemView.findViewById(R.id.nonCriticalIssuesListView);
-            textView.setText(numNonCrit);
-            textView  = (TextView) itemView.findViewById(R.id.inspectionDateListView);
-            textView.setText(inspectDate);
+
+
+            TextView criticalIssuesListViewTextView  = (TextView) itemView.findViewById(R.id.criticalIssuesListView);
+            TextView nonCriticalIssuesListViewTextView  = (TextView) itemView.findViewById(R.id.nonCriticalIssuesListView);
+            TextView inspectionDateListViewTextView  = (TextView) itemView.findViewById(R.id.inspectionDateListView);
+            ImageView inspectionDotColorListViewImageView = (ImageView) itemView.findViewById(R.id.inspectionDotColorListView);
+
+            criticalIssuesListViewTextView.setText(numCrit);
+            nonCriticalIssuesListViewTextView.setText(numNonCrit);
+            inspectionDateListViewTextView.setText(inspectDate);
+            if (currentInspection.getHazardRating() == "Low"){
+              inspectionDotColorListViewImageView.setImageResource(R.drawable.greendot);
+            }
+            else if (currentInspection.getHazardRating() == "Moderate"){
+                inspectionDotColorListViewImageView.setImageResource(R.drawable.yellowdot);
+            }
+            else {
+                inspectionDotColorListViewImageView.setImageResource(R.drawable.reddot);
+            }
+
             return itemView;
         }
 
