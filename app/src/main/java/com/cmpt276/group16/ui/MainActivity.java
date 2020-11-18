1e366769eae10c9c0d37a7a36a453bbde3c3d3ab
William Tjandra <williamtj2001@gmail.com>
2020-11-18 11:38:12 -0800

    -parser fixed for issues/ violump

--- app/src/main/java/com/cmpt276/group16/ui/MainActivity.java
+++ app/src/main/java/com/cmpt276/group16/ui/MainActivity.java
@@ -9,6 +9,7 @@ import android.Manifest;
 import android.app.Activity;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.MenuItem;
